f5d649ae7330266f604723efdebc7eebe40c6f9c
Paul Kling <pkling@gmail.com>
2012-05-23 00:46:06 -0500

    commit for tomorrow

--- src/main/java/com/test/MessageHandler.java
+++ src/main/java/com/test/MessageHandler.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 
 public class MessageHandler implements MessageListener {
-
+	
     @Override
     public void onMessage(Message message) {
         System.out.println("Received message: " + message);
