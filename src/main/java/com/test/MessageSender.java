e471d2300d7a840570407a822a0234651ebd0c16
Paul Kling <pkling@gmail.com>
2012-05-22 23:23:35 -0500

    Intial load

---
+++ src/main/java/com/test/MessageSender.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+
+public class MessageSender {
+
+    @Autowired
+    private AmqpTemplate template;
+
+    @ManagedOperation
+    public void send(String text) {
+        send("NDPAR.SPRING.JAVA", text);
+    }
+
+    @ManagedOperation
+    public void send(String key, String text) {
+        template.convertAndSend(key, text);
+    }
+}
